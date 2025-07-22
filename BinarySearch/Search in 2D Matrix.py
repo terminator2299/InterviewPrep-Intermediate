2b1ef8855050a909df9b92fdacc9396d81afa0e3
terminator2299 <bhavya.code18@gmail.com>
2025-07-22 17:26:40 +0530

    Easy but not so easy q.

---
+++ BinarySearch/Search in 2D Matrix.py
@@ -0,0 +1,45 @@
+# Optimal Solution
+from typing import List
+class Solution:
+
+	def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+		m = len(matrix)
+
+		n = len(matrix[0])
+
+		t = m * n
+
+		l = 0
+
+		r = t - 1
+
+		while l <= r:
+
+			mid = (l + r) // 2
+
+			mid_i = mid // n
+
+			mid_j = mid % n
+
+			mid_num = matrix[mid_i][mid_j]
+
+			if target == mid_num:
+
+				return True
+
+			elif target < mid_num:
+
+				r = mid - 1
+
+			else:
+
+				l = mid + 1
+
+		return False
+
+# Time Complexity: O(log(m * n))
+
+# Space Complexity: O(1)
+
+	 
